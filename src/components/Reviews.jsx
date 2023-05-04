42d4496a584dd34e81e0641802edc7242eff19dd
mdchad <irsyad.muhd@gmail.com>
2023-05-04 15:49:33 +0800

    change copy

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -238,10 +238,10 @@ export function Reviews() {
           id="reviews-title"
           className="text-3xl font-medium tracking-tight text-gray-900 sm:text-center"
         >
-          Everyone is changing their life with Pocket.
+          Learn more at your fingertips.
         </h2>
         <p className="mt-2 text-lg text-gray-600 sm:text-center">
-          Thousands of people have doubled their net-worth in the last 30 days.
+          Start benefiting from the wisdom of the Prophet Muhammad (peace be upon him) in the language of your choice.
         </p>
         <ReviewGrid />
       </Container>
