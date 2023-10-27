445dc74873fda8eac62ed9752b0dff8054f842ca
mdchad <irsyad.muhd@gmail.com>
2023-10-27 20:21:32 +0800

    fix

--- apps/nextjs/src/pages/[bookId]/index.tsx
+++ apps/nextjs/src/pages/[bookId]/index.tsx
@@ -1,8 +1,6 @@
 // @ts-nocheck
 import {appRouter} from "@acme/api";
 import { useRouter } from 'next/router'
-import prisma from '@acme/db/index.ts";
-
 
 import { createServerSideHelpers } from '@trpc/react-query/server';
 import superjson from 'superjson';
