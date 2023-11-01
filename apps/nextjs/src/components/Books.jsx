3ef5c010223ea173f775a2d1a56985ed9df4ac6c
mdchad <irsyad.muhd@gmail.com>
2023-11-01 20:10:10 +0800

    remove button

--- apps/nextjs/src/components/Books.jsx
+++ apps/nextjs/src/components/Books.jsx
@@ -19,7 +19,7 @@ export function Books({ books }) {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-32 mb-20 py-4 sm:py-6 lg:py-16 bg-gray-100">
       {/*<input onChange={(e) => setSearch(e.target.value)} value={search} />*/}
-      <button onClick={handleSubmit}>Submit</button>
+      {/*<button onClick={handleSubmit}>Submit</button>*/}
       <div className="bg-royal-blue mb-12 py-4 px-2 rounded-md">
         <p className="text-2xl font-bold text-white text-center py-4 font-sans">My Way: Hadiths of Prophet Muhammad</p>
       </div>
