d1a13d88cc1c94b1caf413695a8586a8291d458b
mdchad <irsyad.muhd@gmail.com>
2023-11-03 16:16:50 +0800

    add terms and privacy back

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,6 +1,7 @@
 import connectToDatabase from "@/lib/mongodb";
 import Link from "next/link";
 import { ChevronRightSquare } from "lucide-react";
+import Pagination from "@/components/Pagination";
 
 async function getData(terms, page, limit = 10) {
   const db = await connectToDatabase();
@@ -21,11 +22,17 @@ async function getData(terms, page, limit = 10) {
       }
     },
     {
-      $skip: skip
-    },
-    {
-      $limit: limit
-    },
+      $facet: {
+        documents: [
+          { $skip: skip }, // Skip documents for previous pages
+          { $limit: limit }, // Limit to 'limit' number of documents for the current page
+          // Include additional $project here if you want to shape the results
+        ],
+        totalCount: [
+          { $count: "count" } // Count the total number of matching documents
+        ]
+      }
+    }
     // {
     //   $project: {
     //     "content.ms": 1,
@@ -40,27 +47,31 @@ async function getData(terms, page, limit = 10) {
 
 export default async function Search({ searchParams }) {
   const result = await getData(searchParams.term, searchParams.page)
+  const [{ documents, totalCount: [{count}] } ] = result
 
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-32 mb-20 py-4 sm:py-6 lg:py-12">
       <div className="bg-royal-blue py-6 px-4">
         <p className="text-xl font-bold text-white">Search Result: {searchParams.term}</p>
       </div>
+      <div className="grid grid-cols-1 justify-items-end">
+        <Pagination count={count}/>
+      </div>
       <div className="mt-12 p-4 bg-gray-100 grid gap-2">
-        {result.map(data => {
+        {documents.map(data => {
           const id = data._id
           return (
             // <Link key={id} href={`/${params.bookId}/${vol.id}`}>
               <div key={id}>
                 <div className="flex items-center gap-2 pt-4 pb-2">
-                  <Link href={`/${data.book_id}`}><p className="text-royal-blue hover:underline font-sans text-sm font-semibold">{data.book_title.ms}</p></Link>
+                  <Link href={`/${data.book_id}`}><p className="text-royal-blue hover:underline font-sans text-sm font-semibold">{data.book_title?.ms}</p></Link>
                   <span className="text-xs"><ChevronRightSquare color="black" size={18} /></span>
-                  <Link href={`/${data.book_id}/${data.volume_id}`}><p className="text-royal-blue hover:underline font-sans text-sm font-semibold">{data.volume_title.ms}</p></Link>
+                  <Link href={`/${data.book_id}/${data.volume_id}`}><p className="text-royal-blue hover:underline font-sans text-sm font-semibold">{data.volume_title?.ms}</p></Link>
                   <span className="text-xs"><ChevronRightSquare color="black" size={18} /></span>
                   <Link href={`/${data.book_id}/${data.volume_id}#${data.number}`}><p className="text-royal-blue hover:underline font-sans text-sm font-semibold">{data.number}</p></Link>
                 </div>
                 {
-                  data.content.map((content, i) => {
+                  data.content?.map((content, i) => {
                     return (
                       <div key={i} style={{ gridTemplateColumns: "repeat(auto-fit, minmax(320px, 1fr))"}} className={`gap-12 grid p-8 bg-white shadow-sm`}>
                         <p className="text-md text-justify whitespace-pre-line font-arabicSymbol">{content.ms}</p>
--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -2,26 +2,57 @@
 
 import React from 'react';
 import Link from "next/link";
+import {useSearchParams} from "next/navigation";
 
-const Pagination = ({ currentPage, totalPages, paginate }) => {
-  const pageNumbers = [];
+const Pagination = ({ count }) => {
+  const searchParams = useSearchParams();
+  const term = searchParams.get('term');
+  const currentPage = parseInt(searchParams.get("page"), 10) || 1;
 
-  for (let i = 1; i <= Math.ceil(totalPages); i++) {
+  const totalCount = count; // Replace with the actual total count of collections
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(totalCount / itemsPerPage);
+  const maxPageNumberWindow = 5;
+  const startIndex = (currentPage - 1) * itemsPerPage + 1;
+  const endIndex = Math.min(startIndex + itemsPerPage - 1, totalCount);
+
+  let pageNumbers = [];
+  let startPage = Math.max(1, currentPage - Math.floor(maxPageNumberWindow / 2));
+  let endPage = Math.min(totalPages, startPage + maxPageNumberWindow - 1);
+
+  if (endPage - startPage + 1 < maxPageNumberWindow) {
+    startPage = Math.max(1, endPage - maxPageNumberWindow + 1);
+  }
+
+  if (startPage > 1) {
+    pageNumbers = [1, '...'];
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
     pageNumbers.push(i);
+  }
+
+  if (endPage < totalPages) {
+    pageNumbers.push('...');
+    pageNumbers.push(totalPages);
   }
 
   return (
     <nav>
+      <p className="text-right my-2 font-sans text-sm">Showing {startIndex} - {endIndex} of {totalCount}</p>
       <ul className='flex list-none'>
-        {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <Link
-              onClick={() => paginate(number)}
-              href='!#'
-              className={`page-link ${currentPage === number ? 'bg-blue-500 text-white' : 'text-blue-500'} px-4 py-2 mx-1 rounded`}
-            >
-              {number}
-            </Link>
+        {pageNumbers.map((number, i) => (
+          <li key={i}>
+            {number === '...' ? (
+              <span className="px-4 py-2 mx-1">{number}</span>
+            ) : (
+              <Link
+                href={`/search?term=${encodeURIComponent(term)}&page=${number}`}
+                className={`${currentPage === number ? 'bg-royal-blue text-white' : 'hover:bg-gray-100 text-black'} shadow-md text-sm px-2 py-1 mx-1 rounded`}
+              >
+                {number}
+              </Link>
+            )}
           </li>
         ))}
       </ul>
