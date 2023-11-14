ca73c81bca8f5baa3fa3528573265421d57ffaf0
mdchad <irsyad.muhd@gmail.com>
2023-11-14 21:34:36 +0800

    fix error

--- app/api/books/[bookId]/[volumeId]/route.js
+++ app/api/books/[bookId]/[volumeId]/route.js
@@ -2,11 +2,13 @@ import connectToDatabase from "@/lib/mongodb";
 import {NextRequest, NextResponse} from "next/server";
 
 
-export async function GET({ params }) {
+export async function GET(req) {
+  const params = req.params
   const db = await connectToDatabase();
   const bookId = params.bookId
+  const volumeId = params.volumeId
 
-  const data = await db.collection('Volumes').find({book_id: bookId}).toArray();
+  const data = await db.collection('Hadiths').find({ volume_id: volumeId, book_id: bookId }).sort({ number: 1 }).toArray();
 
   return NextResponse.json({
     success: true,
--- app/api/books/[bookId]/route.js
+++ app/api/books/[bookId]/route.js
@@ -2,12 +2,13 @@ import connectToDatabase from "@/lib/mongodb";
 import {NextRequest, NextResponse} from "next/server";
 
 
-export async function GET({ params }) {
+export async function GET(req) {
+  const params = req.params
+
   const db = await connectToDatabase();
   const bookId = params.bookId
-  const volumeId = params.volumeId
 
-  const data = await db.collection('Hadiths').find({ volume_id: volumeId, book_id: bookId }).sort({ number: 1 }).toArray();
+  const data = await db.collection('Volumes').find({book_id: bookId}).toArray();
 
   return NextResponse.json({
     success: true,
