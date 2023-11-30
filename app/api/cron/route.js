34c56f7cecc17ed4d2a989067687dcd74c2a9f8e
mdchad <irsyad.muhd@gmail.com>
2023-11-30 12:09:38 +0800

    log cron

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -5,7 +5,9 @@ import { kv } from '@vercel/kv'
 export async function GET(request) {
   const db = await connectToDatabase();
 
+  console.log('hit cron')
   const hadith = await db.collection('Hadiths').aggregate([{ $sample: { size: 1 } }]).toArray();
+  console.log('hadith', hadith)
 
   // Cache the Hadith in Vercel KV
   await kv.set('todayHadith', hadith[0]);
