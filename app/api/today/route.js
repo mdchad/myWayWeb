191ef3bb285849c684f45e2822832b629afe5e5c
mdchad <irsyad.muhd@gmail.com>
2023-12-01 10:22:56 +0800

    add more logs

--- app/api/today/route.js
+++ app/api/today/route.js
@@ -2,7 +2,9 @@ import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  console.log('this is today')
   const hadith = await kv.get('todayHadith');
   console.log(hadith)
   return NextResponse.json(hadith);
 }
+export const dynamic = "force-dynamic";
