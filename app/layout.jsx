55ba70f420ca6f1442a803ec5bc87470b0725110
mdchad <irsyad.muhd@gmail.com>
2023-12-26 12:42:24 +0800

    remove clerk

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,6 @@ import {arabicFont, inter, arabicSymbolFont} from "@/app/font";
 import {Footer} from "@/components/Footer";
 import {Header} from "@/components/Header";
 import NextTopLoader from 'nextjs-toploader';
-import {ClerkProvider} from "@clerk/nextjs";
 
 export const metadata = {
   title: 'My Way',
@@ -12,7 +11,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
       <html lang="en" className={`${inter.variable} ${arabicFont.variable} ${arabicSymbolFont.variable}`} content="text/html; charset=utf-8">
         <body>
           <NextTopLoader />
@@ -21,6 +19,5 @@ export default function RootLayout({ children }) {
           <Footer />
         </body>
       </html>
-    </ClerkProvider>
   )
 }
