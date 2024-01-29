a0849eeb17fe91258c40855fd4f31bfd22debddb
mdchad <irsyad.muhd@gmail.com>
2024-01-29 13:09:53 +0800

    add on web

--- app/[bookId]/[volumeId]/page.jsx
+++ app/[bookId]/[volumeId]/page.jsx
@@ -8,7 +8,7 @@ export const fetchCache = 'force-no-store'
 
 async function getData(volumeId, bookId) {
   const db = await connectToDatabase();
-  if (bookId === "240360e4-50b4-47a9-9506-9850b0e3bfd7") { //check for muslim because they have different way of sort
+  if (bookId === "240360e4-50b4-47a9-9506-9850b0e3bfd7" && volumeId !== "2b1bc287-cdea-4e51-b5d3-f6fa0ce31235") { //check for muslim because they have different way of sort
     return await db.collection('Hadiths').find({ volume_id: volumeId, book_id: bookId }).sort({ _id: 1 }).toArray();
   }
   return await db.collection('Hadiths').find({ volume_id: volumeId, book_id: bookId }).sort({ number: 1 }).toArray();
