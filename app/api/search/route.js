c0d3e5d0a17a275f5a6f7c37754568884cf43535
mdchad <irsyad.muhd@gmail.com>
2024-03-07 15:47:13 +0800

    modify search api

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -9,22 +9,45 @@ export async function GET(request) {
   const page = Number(searchParams.get('page'))
   const limit = Number(searchParams.get('limit'))
   const query = searchParams.get('query')
+  const books = searchParams.get('books')
 
   const skip = (page - 1) * limit;
 
-  const cursor = await db.collection('Hadiths').aggregate([
-    {
-      $search: {
-        index: "content",
-        text: {
-          query,
-          path: ['content.ms', 'content.ar']
-        },
-        highlight: {
-          path: ['content.ms', 'content.ar'],
-        }
+  let searchQuery = {
+    $search: {
+      index: 'content',
+      compound: {
+        must: [
+          {
+            text: {
+              query: query,
+              // path: ['content', 'volumeTitle'] // Search in both content and volume title
+              path: ['content.ms', 'content.ar']
+            }
+          }
+        ],
+        filter: [],
+        // minimumShouldMatch: 1 // Adjust based on your filtering logic
       }
-    },
+    }
+  };
+
+  if (books.length > 0) {
+    books.forEach(book => {
+      searchQuery.$search.compound.filter.push({
+        text: {
+          path: 'book_name', // Assuming 'title' is the field with the book title
+          query: book
+        }
+      });
+    });
+  } else {
+    // If no specific books are selected, adjust minimumShouldMatch
+    delete searchQuery.$search.compound.filter;
+  }
+
+  const cursor = await db.collection('Hadiths').aggregate([
+    searchQuery,
     {
       $facet: {
         documents: [
