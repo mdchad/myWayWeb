feeafb5a13da1c717d2f9006068f44a39c4f0dc8
mdchad <irsyad.muhd@gmail.com>
2024-06-27 12:39:55 +0800

    fix malay word

--- components/Header.jsx
+++ components/Header.jsx
@@ -168,7 +168,7 @@ export function Header() {
                 value={searchTerm}
                 onChange={handleChange}
                 type="search"
-                placeholder="Cari..."
+                placeholder="Carian..."
                 className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
               />
             </div>
