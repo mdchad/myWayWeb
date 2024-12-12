b912ace32d15cbee43988b7745654ecfbf9c3f39
mdchad <irsyad.muhd@gmail.com>
2024-12-12 14:57:00 +0800

    trying alias import again

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react"
 
 // @ts-ignore
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000" // the base url of your auth server
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL // the base url of your auth server
 })
