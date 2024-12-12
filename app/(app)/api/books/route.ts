512a7efa5bfbbfe73b4e1c954307ca4e8b523fe8
Muhammad Irsyad <irsyad.muhd@gmail.com>
2024-12-12 17:45:32 +0800

    Dev (#19)
    * add bar
    
    * enchance stuff
    
    * added cn
    
    * fix url navigation
    
    * fix hadith cron
    
    * fix refresh issue on today card
    
    * fix prettier
    
    * add highlighting
    
    * add volume get
    
    * refactor
    
    * add animation
    
    * revert to previous solution
    
    * format pretier
    
    * fix suspense issue
    
    * remove log
    
    * conditional rendering
    
    * clean up
    
    * log node env
    
    * change name
    
    * change env name
    
    * add all
    
    * clean up
    
    * change title
    
    * update copy
    
    * fix copy paste, fix mobile view
    
    * fix endpoint
    
    * fix endpoint
    
    * fix slug issue
    
    * fix og, fix title, fix search sunspense
    
    * remove cache file
    
    * add pdf
    
    * change copy
    
    * fix audio mixup
    
    * add pdf, add new path for book to avoid slug collision
    
    * refer to prev commit
    
    * add play store logo to static
    
    * upgarde to nextjs 15, react, fix issues
    
    * added back canvas
    
    * added auth, better-auth
    
    * add maintenance mode
    
    * convert js files to ts
    
    * fix path
    
    * fix types
    
    * Versioning untracked files
    
    * add baseurl
    
    * add path explicitly
    
    * convert ts file
    
    * fix base url
    
    * update config
    
    * remove canvas config
    
    * add back canvas
    
    * fix tailwind
    
    * test relative path
    
    * check import
    
    * replaced all
    
    * fix import
    
    * add typescript
    
    * trying alias import again
    
    * back to alias import
    
    * add no check
    
    * block access to api

--- app/(app)/api/books/route.ts
+++ app/(app)/api/books/route.ts
@@ -1,8 +1,34 @@
 import connectToDatabase from "@/lib/mongodb";
 import { NextResponse } from "next/server";
+import {headers} from "next/headers";
+import {auth} from "@/lib/auth";
+
+const emptyData = {
+  "success": true,
+  "data": [
+    {
+      "_id": "65377edf87c0a6c62eec8b92",
+      "name": "access_denied",
+      "title": "Access Denied",
+      "id": "ad6a2cc8-f34b-476b-9b7e-6756a3b7d43f",
+      "slug": "denied"
+    }
+  ]
+}
 
 export async function GET() {
+  const session = await auth.api.getSession({
+    headers: await headers() // you need to pass the headers object.
+  })
+
   const db = await connectToDatabase();
+
+  if (!session) {
+    return NextResponse.json({
+      ok: false,
+      data: emptyData
+    });
+  }
 
   const data = await db.collection("Books").find({}).sort({ _id: 1 }).toArray();
   return NextResponse.json({
